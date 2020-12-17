80bd025e09589d83a63b3a5abf1926369cad6644
Vipul-Cariappa <vipulcariappa@gmail.com>
2020-12-17 12:13:32 +0530

    button for test and submit; might be broken

--- coder/challenge/models.py
+++ coder/challenge/models.py
@@ -5,10 +5,14 @@ from django.contrib.auth.models import User
 
 
 class Question(models.Model):
+    title = models.CharField(max_length=100, unique=True)
     question = models.TextField()
     solution = models.TextField()
     test_case = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Answer(models.Model):
@@ -18,3 +22,6 @@ class Answer(models.Model):
     memory = models.IntegerField()
     character = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}-{self.question.title}"
--- coder/challenge/urls.py
+++ coder/challenge/urls.py
@@ -7,6 +7,7 @@ app_name = "challenge"
 urlpatterns = [
     path('', views.index, name='home'),
     path('new-question', views.question_new, name='new-question'),
+    path('list-question', views.question_list, name='list-question'),
     path('test-answer/<int:question_id>',
          views.answer_test, name='new-question'),
     path('answer/<int:question_id>', views.answer, name='answer'),
